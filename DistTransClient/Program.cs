2b2798f5f4b39b852d7cc9278c16d87adf0ec9a0
dth <dengtaihua@dxn.com.cn>
2017-10-17 17:57:25 +0800

    增加DTC服务类

--- DistTransClient/Program.cs
+++ DistTransClient/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DistTransServices;
+using PWMIS.EnterpriseFramework.Service.Basic;
+using PWMIS.EnterpriseFramework.Service.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,72 @@ namespace DistTransClient
     {
         static void Main(string[] args)
         {
+            DistTrans3PCState state ;//= DistTrans3PCState.Rep_Yes_1PC;
+            if (typeof(DistTrans3PCState).IsEnum)
+            {
+                state = (DistTrans3PCState)Enum.Parse(typeof(DistTrans3PCState), "Rep_Yes_1PC");
+                
+            }
+
+            Proxy client = new Proxy();
+            client.ErrorMessage += client_ErrorMessage;
+            Console.Write("请输入服务器的主机名或者IP地址(默认 127.0.0.1)：");
+            string host = Console.ReadLine();
+            if (string.IsNullOrEmpty(host))
+                host = "127.0.0.1";
+            Console.WriteLine("服务地址：{0}", host);
+
+            Console.Write("请输入服务的端口号(默认 8888)：");
+            string port = Console.ReadLine();
+            if (string.IsNullOrEmpty(port))
+                port = "8888";
+            Console.WriteLine("服务端口号：{0}", port);
+
+            client.ServiceBaseUri = string.Format("net.tcp://{0}:{1}", host, port);
+            Console.WriteLine("当前客户端代理的服务基础地址是：{0}", client.ServiceBaseUri);
+            Console.WriteLine();
+            Console.WriteLine("MSF 分布式事务 模式调用示例：");
+
+            ServiceRequest request = new ServiceRequest();
+            request.ServiceName = "DTCService";
+            request.MethodName = "RegisterTransaction";
+            request.Parameters = new object[] { "1234567890" };
+
+            client.Subscribe<string, DistTrans3PCState, DistTrans3PCState>(request,
+                s =>
+                {
+                    Console.WriteLine("ServerMessage:{0}", s);
+                    client.SendTextMessage("client ....");
+
+                },
+                para => {
+                    Console.WriteLine("服务器回调消息：{0}",para);
+                    Console.Write("事务是否执行成功？(yes/no)");
+                    string repMsg1 = Console.ReadLine();
+                    if (repMsg1 == "yes")
+                        return DistTrans3PCState.Rep_Yes_1PC;
+                    else
+                        return DistTrans3PCState.Rep_No_1PC;
+                }
+            );
+
+            System.Threading.Thread.Sleep(1000 * 60 * 60);
+            string repMsg = "ok";
+            while (repMsg != "")
+            {
+                Console.Write("回复服务器(输入为空，则退出)：>>");
+                repMsg = Console.ReadLine();
+                client.SendTextMessage(repMsg);
+            }
+
+          
+            Console.ReadLine();
+
+        }
+
+        static void client_ErrorMessage(object sender, MessageSubscriber.MessageEventArgs e)
+        {
+            Console.WriteLine("请求服务器错误：{0}", e.MessageText);
         }
     }
 }
---
+++ DistTransServices/DTCService.cs
@@ -0,0 +1,122 @@
+﻿using PWMIS.EnterpriseFramework.Service.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistTransServices
+{
+    class DistTransInfo
+    {
+        public string ClientIdentity { get; set; }
+        public string TransIdentity { get; set; }
+        public DistTrans3PCState CurrentDTCState { get; set; }
+        /// <summary>
+        /// 上次状态改变的时间
+        /// </summary>
+        public DateTime LastStateTime { get; set; }
+
+    }
+
+    /// <summary>
+    /// 分布式事务控制器
+    /// </summary>
+    class DTController
+    {
+        public string TransIdentity { get; set; }
+        public DTController(string identity)
+        {
+            TransIdentity = identity;
+        }
+
+        public static DTController CheckStartController(string transIdentity)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 获取分布式事务的状态，注意某个调用线程可能会被阻塞等待
+        /// </summary>
+        /// <param name="resourceState">资源服务器的状态</param>
+        /// <returns></returns>
+        public DistTrans3PCState GetDTCState(DistTrans3PCState resourceState)
+        {
+            if (resourceState == DistTrans3PCState.Rep_No_1PC)
+            {
+                return DistTrans3PCState.Abort;
+            }
+            else
+            {
+
+            }
+
+            return DistTrans3PCState.CanCommit;
+        }
+    }
+
+    /// <summary>
+    /// 分布式事务协调器服务，基于3PC过程。
+    /// </summary>
+    public class DTCService:ServiceBase
+    {
+        private int TransactionResourceCount;
+        private DistTrans3PCState CurrentDTCState;
+
+        private static System.Collections.Concurrent.ConcurrentBag<DistTransInfo> DTResourceList = new System.Collections.Concurrent.ConcurrentBag<DistTransInfo>();
+
+        /// <summary>
+        /// （资源服务器向协调器服务）注册事务操作
+        /// </summary>
+        /// <param name="identity">事务标识，需要在一个分布式事务下的服务请求必须使用同一个事务标识</param>
+        /// <returns></returns>
+        public ServiceEventSource RegisterTransaction(string identity)
+        {
+            //事务计数器累加
+            System.Threading.Interlocked.Increment(ref TransactionResourceCount);
+           
+            return new ServiceEventSource(new object(),5, () => {
+                //注册即向资源服务器发出 CanCommit请求
+                //base.CurrentContext.PublishData(DistTrans3PCState.CanCommit);
+                CurrentDTCState = DistTrans3PCState.CanCommit;
+                while (CurrentDTCState != DistTrans3PCState.Completed)
+                {
+                    CurrentDTCState = base.CurrentContext.CallBackFunction<DistTrans3PCState, DistTrans3PCState>(CurrentDTCState);
+                    Console.WriteLine("Callback Message:{0}", CurrentDTCState);
+                }
+             
+            });
+        }
+
+        public bool BeginTransaction(string identity)
+        {
+            DistTransInfo info = new DistTransInfo();
+            info.ClientIdentity = base.CurrentContext.Request.ClientIdentity;
+            info.CurrentDTCState = DistTrans3PCState.CanCommit;
+            info.LastStateTime = DateTime.Now;
+            info.TransIdentity = identity;
+            DTResourceList.Add(info);
+            //获取一个当前事务标识的协调器线程
+            DTController controller = DTController.CheckStartController(identity);
+
+            CurrentDTCState = DistTrans3PCState.CanCommit;
+            while (CurrentDTCState != DistTrans3PCState.Completed)
+            {
+                //获取资源服务器的事务状态
+                info.CurrentDTCState = base.CurrentContext.CallBackFunction<DistTrans3PCState, DistTrans3PCState>(CurrentDTCState);
+                CurrentDTCState = controller.GetDTCState(info.CurrentDTCState);
+               
+                Console.WriteLine("Callback Message:{0}", CurrentDTCState);
+            }
+            return true;
+        }
+
+        
+
+
+        public override bool ProcessRequest(IServiceContext context)
+        {
+            return base.ProcessRequest(context);
+        }
+    }
+}
